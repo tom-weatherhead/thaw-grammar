2136aaeec302e833a0aadbe1fc2997cdfb5b534c
Tom Weatherhead <tomw@2hrd4u.org>
2021-09-23 11:59:27 -0600

    Dev

--- src/languages/lambda-calculus/operators.ts
+++ src/languages/lambda-calculus/operators.ts
@@ -38,6 +38,16 @@ export function createValueFalse(options: { x?: string; y?: string } = {}): ILCE
 	return l(x, l(y, y));
 }
 
+// I.e. let var = e1 in e2
+
+export function createStatementLetUsage(
+	vv: LCVariable,
+	e1: ILCExpression,
+	e2: ILCExpression
+): ILCExpression {
+	return c(l(vv, e2), e1);
+}
+
 export function createOperatorIfUsage(
 	condition: ILCExpression,
 	thenPart: ILCExpression,
@@ -192,3 +202,163 @@ export function createOperatorMultiply(
 // export function createOperator(options: {} = {}): ILCExpression {
 // 	;
 // }
+
+// **** Pairs (Tuples) ****
+
+// From https://code.iamkate.com/lambda-calculus/tuples-and-lists/ :
+
+// Tuples and lists in lambda calculus
+//
+// Tuples
+//
+// A tuple is a pair of functions. Using the lambda expressions below, a pair of a and b can be made using ‘pair a b’, and the first and second functions of p can be extracted using ‘first p’ and ‘second p’ respecively.
+//
+//     pair = λabf.fab -> λa.λb.λf.((f a) b) -> I.e. if
+// 		- ((pair a) b) -> λf.((f a) b)
+//     first = λp.p(λab.a) -> λp.(p (λa.λb.a)) -> I.e. λp.(p TRUE)
+//     second = λp.p(λab.b) -> λp.(p (λa.λb.b)) -> I.e. λp.(p FALSE)
+
+export function createPairUsage(
+	first: ILCExpression,
+	second: ILCExpression,
+	options: { f?: string } = {}
+): ILCExpression {
+	const f = v(options.f, 'f');
+
+	return l(f, c(c(f, first), second));
+}
+
+export function createFunctionCreatePair(
+	options: { f?: string; a?: string; b?: string } = {}
+): ILCExpression {
+	const a = v(options.a, 'a');
+	const b = v(options.b, 'b');
+
+	return l(a, l(b, createPairUsage(a, b, options)));
+}
+
+export function createFunctionGetFirstOfPair(
+	options: { x?: string; y?: string } = {}
+): ILCExpression {
+	return createValueTrue(options);
+}
+
+export function createFunctionGetSecondOfPair(
+	options: { x?: string; y?: string } = {}
+): ILCExpression {
+	return createValueFalse(options);
+}
+
+// **** Lists ****
+
+// A list is either empty, or consists of a head (any lambda expression) and a tail (another list). The most elegant way of representing a list is based on the representation of integer. The list containing a1, a2... ...an, is represented by λfx.fa1(fa2(...fan-1(fanx)...)). The lambda expression for the empty list, append function, head function and test for the empty list are:
+//
+// 	-? λf.λx.((f a1) (((f a2) (...((f an-1) ((f an) x)) ...))))
+//
+//     empty (i.e. the empty list?) = λf.λx.x -> I.e. the Church numeral zero
+//     append = λa.λl.λf.λx.((f a) (((l f) x)))
+//     head = λl.((l (λa.λb.a)) (any expression)) -> Gets the head of a list?
+//     isempty = λl.((l (λa.λb.FALSE)) TRUE)
+//
+// ((append a) l) constructs a list with head a and tail l.
+
+export function createEmptyList(options: { f?: string; x?: string } = {}): ILCExpression {
+	// This is equivalent to the Church numeral zero.
+	const f = v(options.f, 'f');
+	const x = v(options.x, 'x');
+
+	return l(f, l(x, x));
+}
+
+export function createFunctionAppend(
+	options: { a?: string; l?: string; f?: string; x?: string } = {}
+): ILCExpression {
+	// append = λa.λl.λf.λx.((f a) ((l f) x))
+	const a = v(options.a, 'a');
+	const ll = v(options.l, 'l'); // Var named ll because of the func l() above.
+	const f = v(options.f, 'f');
+	const x = v(options.x, 'x');
+
+	return l(a, l(ll, l(f, l(x, c(c(f, a), c(c(ll, f), x))))));
+}
+
+export function createFunctionGetHeadOfList(options: { l?: string } = {}): ILCExpression {
+	// head = λl.((l (λa.λb.a)) (any expression))
+	const ll = v(options.l, 'l');
+
+	return l(ll, c(c(ll, createValueTrue()), ll));
+}
+
+export function createFunctionIsListEmpty(
+	options: { a?: string; b?: string; l?: string } = {}
+): ILCExpression {
+	// isempty = λl.((l (λa.λb.FALSE)) TRUE)
+	const a = v(options.a, 'a');
+	const b = v(options.b, 'b');
+	const ll = v(options.l, 'l');
+
+	return l(ll, c(c(ll, l(a, l(b, createValueFalse()))), createValueTrue()));
+}
+
+// The tail function is more complicated, and makes use of the tuples defined above. The principle is to start off with a pair (empty, empty), and at each stage turn the pair (x, y) into the pair (y, ((append a) y)), where a is the current list element, and then take the first element of the pair. The lambda expression is:
+//
+//     tail = λl.first(l(λab.pair(second b)(append a (second b)))(pair empty empty))
+// 		-> λl.(first (l (λa.λb.(pair (second b)) ((append a) (second b))) ((pair empty) empty)))
+
+export function createFunctionGetTailOfList(
+	options: { l?: string; a?: string; b?: string } = {}
+): ILCExpression {
+	// tail = λl.first(
+	//   l(λab.pair(second b)(append a (second b)))
+	//   (pair empty empty)
+	// )
+	// -> λl.(first (l
+	//   (
+	//     λa.λb.(pair (second b))
+	//     ((append a) (second b))
+	//   )
+	//   ((pair empty) empty)))
+	const ll = v(options.l, 'l');
+
+	const cp = createFunctionCreatePair();
+	const f = createFunctionGetFirstOfPair();
+	const s = createFunctionGetSecondOfPair();
+	const e = createEmptyList();
+	const ap = createFunctionAppend();
+
+	const a = v(options.a, 'a');
+	const b = v(options.b, 'b');
+
+	return l(
+		ll,
+		c(
+			f,
+			c(
+				ll, // Calls No. 1 and 2
+				c(
+					// BEGIN Call No. 3
+					// BEGIN Callee No. 3
+					l(
+						a,
+						l(
+							b,
+							c(
+								// Create a pair : Takes 2 args, so make 2 calls
+								c(cp, c(s, b)), // The pair's first element
+								c(c(ap, a), c(s, b))
+							)
+						) // The pair's second element
+					), // END of creation of pair
+					// END Callee No. 3
+					c(c(cp, e), e) // Argument for call No. 3
+				) // END Call No. 3
+			)
+		)
+	);
+}
+
+// - In LISP terms:
+// 	- head = car
+// 	- tail = cdr
+// 	- cons? = append? -> It looks like append adds a onto the head of l, not the tail.
+// 		cons = λa.λl.λf.λx.?((f a) (l f) x)
